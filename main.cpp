b40182a599a72d5258911a7d8fdf4607a8ca6d57
Sergey <VareskoSK@mpei.ru>
2020-06-03 19:39:55 +0300

    code: вывод гистограммы чёрными прямоугольниками

--- main.cpp
+++ main.cpp
@@ -126,8 +126,16 @@ void show_histogram_svg(const vector<size_t>& bins)
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    svg_text(TEXT_LEFT, TEXT_LEFT, to_string(bins[0]));
-    svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+    //svg_text(TEXT_LEFT, TEXT_LEFT, to_string(bins[0]));
+    //svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+    double top = 0;
+    for(size_t bin:bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
+    }
     svg_end();
 }
 
