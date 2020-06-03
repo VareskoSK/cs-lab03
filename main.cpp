e3e27f97bc05b6c382db7cbac1cd11252d01caa3
Sergey <VareskoSK@mpei.ru>
2020-06-03 20:02:05 +0300

    build: функция find_minmax вынесена в отдельный модуль

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -12,24 +13,6 @@ vector<double> input_numbers(size_t count)
         cin >> result[i];
     }
  return result;
-}
-
-void find_minmax(vector<double>& numbers, double& min, double& max)
-{
-  min = numbers[0];
-  max = numbers[0];
-    for (double number : numbers)
- {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
- }
- return;
 }
 
 vector<size_t> make_histogram(vector<double> numbers, size_t bin_count)
