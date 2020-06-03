2ed7b4ee81179e8e7df9ee597207dedab931bc38
Sergey <VareskoSK@mpei.ru>
2020-06-03 20:04:53 +0300

    build: функция find_minmax вынесена в отдельный модуль(fix)

---
+++ histogram.cpp
@@ -0,0 +1,22 @@
+#include "histogram.h"
+#include <vector>
+
+using namespace std;
+//this message makes no sense
+void find_minmax(vector<double> numbers, double& min, double& max)
+{
+  min = numbers[0];
+  max = numbers[0];
+    for (double number : numbers)
+ {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+ }
+ return;
+}
---
+++ histogram.h
@@ -0,0 +1,10 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+#include <vector>
+
+using namespace std;
+
+void find_minmax(vector<double> numbers, double& min, double& max);
+//this message makes no sense
+
+#endif // HISTOGRAM_H_INCLUDED
