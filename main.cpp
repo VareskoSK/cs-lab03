c1920da11d0402dce390afa6723f0290ec19ff4b
Sergey <VareskoSK@mpei.ru>
2020-06-04 11:33:37 +0300

    build: создан модуль SVG

--- main.cpp
+++ main.cpp
@@ -1,50 +1,10 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
+#include "SVG.h"
 
 using namespace std;
 
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
-{
-    cout << "<rect x='"<< x << "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<< stroke <<"' fill='"<<fill<<"'/>\n";
-}
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='"<< baseline <<"'>"<<text<<"</text>\n";
-}
-void svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-void svg_end()
-{
-    cout << "</svg>\n";
-}
-void show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-}
 vector<double> input_numbers(size_t count)
 {
     vector<double> result(count);
@@ -55,7 +15,6 @@ vector<double> input_numbers(size_t count)
     }
     return result;
 }
-
 vector<size_t> make_histogram(vector<double> numbers, size_t bin_count)
 {
     vector<size_t> bins(bin_count);
@@ -115,10 +74,14 @@ int main()
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
+    if (number_count==0)
+        return 1;
     const auto numbers = input_numbers(number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
+    if (bin_count==0)
+        return 1;
     const auto bins = make_histogram(numbers, bin_count);
     show_histogram_svg(bins);
     return 0;
--- test.cpp
+++ test.cpp
@@ -1,16 +1,42 @@
 #include "histogram.h"
-
 #include <cassert>
-
 void
-test_positive() {
+test_positive()
+{
     double min = 0;
     double max = 0;
     find_minmax({1, 2, 3}, min, max);
+    find_minmax({ }, min, max);
     assert(min == 1);
     assert(max == 3);
 }
-
-int main() {
+void test_otricala()
+{
+    double min=0;
+    double max=0;
+    find_minmax({-1,-2,-3},min,max);
+    assert(max == -1);
+    assert(min == -3);
+}
+void test_same()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({3, 3, 3}, min, max);
+    assert(min == 3);
+    assert(max == 3);
+}
+void test_ones()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({2}, min, max);
+    assert(min == 2);
+    assert(max == 2);
+}
+int main()
+{
     test_positive();
+    test_ones();
+    test_same();
 }
