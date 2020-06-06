a3049e2b0ad4c04a1734b47907c51d630090b858
Sergey <VareskoSK@mpei.ru>
2020-06-06 20:38:20 +0300

    code: Загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -39,14 +39,21 @@ int main(int argc, char* argv[])
 {
     if (argc > 1)
     {
-        for (int i = 0; i < argc; i++)
+        CURL *curl = curl_easy_init();
+        if(curl)
         {
-            cout << "argv[" << i << "] = " << argv[i] << '\n';
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            if (res)
+            {
+                cerr << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
+            curl_easy_cleanup(curl);
         }
         return 0;
     }
-
-    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input.numbers, input.bin_count);
     show_histogram_svg(bins, input.bin_height, input.bin_count);
